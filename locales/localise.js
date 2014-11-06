fcc2a5a07d60688d2af9f56ce9d1d9539947b4bc
Simon Maxwell-Stewart <smaxwellstewart@gmail.com>
2014-11-06 18:43:25 -0500

    touched up content

---
+++ locales/localise.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+
+function readLines(input, func) {
+var remaining = '';
+
+input.on('data', function(data) {
+  remaining += data;
+  var index = remaining.indexOf('\n');
+  while (index > -1) {
+    var line = remaining.substring(0, index);
+    remaining = remaining.substring(index + 1);
+    func(line);
+    index = remaining.indexOf('\n');
+  }
+});
+
+input.on('end', function() {
+  if (remaining.length > 0) {
+    console.log();
+    func(remaining);
+  }
+});
+}
+
+function func(data) {
+  console.log('Line: ' + data);
+}
+
+var input = fs.createReadStream('content.txt');
+readLines(input, func);
